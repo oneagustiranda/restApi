ef5fd579ea8c3c54733a784f76649c5fcf683078
oneagustiranda <agustiranda.one@gmail.com>
2020-02-14 11:36:46 +0700

    commit awal

---
+++ app/models/db.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql");
+const dbConfig = require("../config/db.config");
+var connection = mysql.createPool({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DATABASE
+});
+module.exports = connection;
